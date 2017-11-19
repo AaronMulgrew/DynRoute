9f21f52511168d8bff3c62c92beba3c71382302f
Aaron <azz@live.ie>
2017-11-19 19:46:13 +0000

    added unit test to test the haversine formula

---
+++ Server/tests/unit_tests.py
@@ -0,0 +1,11 @@
+import unittest
+from scripts import haversine
+
+class test_haversine(unittest.TestCase):
+    def test_haversine(self):
+        ## this is the distance from the two sets of coordinates in metres.
+        distance = 249.6400152128023
+        self.assertEqual(haversine.get_distance_haversine([52.632930, -1.161572], [52.632912, -1.157873]), distance)
+
+if __name__ == '__main__':
+    unittest.main()
