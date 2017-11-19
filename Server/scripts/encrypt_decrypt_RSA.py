06b326a5408b0091cf0669ed3e004b0818cd8adf
Aaron <Aaron@LAPTOP-2AO2MULD>
2017-11-19 18:33:45 +0000

    added encrypt and decrypt functions to RSA.py

--- Server/scripts/encrypt_decrypt_RSA.py
+++ Server/scripts/encrypt_decrypt_RSA.py
@@ -1,12 +1,24 @@
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 
-message = 'To be encrypted'
-key = RSA.importKey(open('public.pem').read())
-cipher = PKCS1_OAEP.new(key)
-ciphertext = cipher.encrypt(message)
-print ciphertext
-key = RSA.importKey(open('private.pem').read(), passphrase='123456')
-cipher = PKCS1_OAEP.new(key)
-message = cipher.decrypt(ciphertext)
-print message
+
+def Encrypt(publicKey, message):
+    #message = 'To be encrypted'
+    key = RSA.importKey(open(publicKey).read())
+    #key = RSA.importKey(open('public.pem').read())
+    cipher = PKCS1_OAEP.new(key)
+    ciphertext = cipher.encrypt(message)
+    print ciphertext
+    return ciphertext
+
+def Decrypt(privateKey, message):
+    key = RSA.importKey(open(privateKey).read(), passphrase='123456')
+    cipher = PKCS1_OAEP.new(key)
+    message = cipher.decrypt(ciphertext)
+    print message
+    return message
+
+publicKey = 'scripts/public.pem'
+message = 'To be Encrypted'
+ciphertext = Encrypt(publicKey, message)
+print Decrypt('scripts/private.pem', ciphertext)
