ea318b8404594d348d7b32acffceaeb0c808f162
Aaron <azz@live.ie>
2017-11-24 16:20:03 +0000

    fix again for TravisCI.

--- Server/tests/unit_tests.py
+++ Server/tests/unit_tests.py
@@ -1,6 +1,7 @@
 import unittest
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'scripts'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import haversine
 import server
 
