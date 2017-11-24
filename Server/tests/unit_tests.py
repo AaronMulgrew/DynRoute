06c988ba627de56c6a04769c641fdac99f76bd7a
Aaron <azz@live.ie>
2017-11-24 20:12:09 +0000

    removed explicit 'from scripts'

--- Server/tests/unit_tests.py
+++ Server/tests/unit_tests.py
@@ -1,7 +1,7 @@
 import unittest
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'scripts'))
-from scripts import haversine
+import haversine
 from .. import server
 
 
